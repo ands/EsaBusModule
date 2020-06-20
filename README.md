55c355ddfb6fad774bba73c5a8cf4f0aa76863a4
ands <amanti@gmx.de>
2020-06-20 22:14:03 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EsaBusModule
+
+This is a repository for all things around the EsaBusModule, an Arduino Nano compatible development board for the ESA 1919 and ESA 5000 electric scooters.
---
+++ firmware/speedswitch/EsaSpeedSwitch.ino
@@ -0,0 +1,22 @@
+// EsaSpeedSwitch
+//
+// This is a demo application for the EsaBusModule, an Arduino Nano compatible development board.
+// Its purpose is to switch the maximum speed of ESA 1919/5000 scooters between 20km/h and 30km/h.
+// Copyright (C) 2020 https://github.com/ands
+// 
+// This project is based off of the work done in https://github.com/M4GNV5/DocGreenESA5000-Dashboard.
+
+// LICENSE
+// 
+// This project is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This project is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this project. If not, see <https://www.gnu.org/licenses/>.
