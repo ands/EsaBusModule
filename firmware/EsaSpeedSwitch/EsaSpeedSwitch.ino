e2c5086f90a8f5bd329c70ca048066abc0f84564
Marco Hein <locomarco@live.de>
2020-07-01 18:48:44 +0200

    synced with rewrite

--- firmware/EsaSpeedSwitch/EsaSpeedSwitch.ino
+++ firmware/EsaSpeedSwitch/EsaSpeedSwitch.ino
@@ -25,32 +25,33 @@
 #include <stdint.h>
 
 #define LED_BUILTIN 13
-#define THRESHOLD 0x80 //(128) min: 0x2b (43) max: 0xc5 (197)
-#define THRESHOLD_LOW 0x40
+#define THRESHOLD_LOW 0x30 // Lever just pushed a little bit
+#define THRESHOLD_HIGH 0x80 // Lever pushed about half way    min: 0x2b (43) max: 0xc5 (197)
 
 #define SPEED_HIGH 30 // km/h
 #define SPEED_MEDIUM 25
 #define SPEED_LOW 20
 
-#define COMMAND_ECO 0x7C
-#define COMMAND_LIGHT 0xF0
-#define COMMAND_LOCK 0x7D
-#define COMMAND_SPEED 0xF2
+#define ADDRESS_22 0x22
+#define COMMAND_01 0x01
+#define ARG_ECO 0x7C
+#define ARG_LIGHT 0xF0
+#define ARG_LOCK 0x7D
+#define ARG_MAXSPEED 0xF2
+
+#define ADDRESS_11 0x11
+#define COMMAND_33 0x33
+#define ARG_SPEED 0x28
 
 #define RX_DISABLE UCSR0B &= ~_BV(RXEN0);
-#define RX_ENABLE  UCSR0B |=  _BV(RXEN0);
+#define RX_ENABLE UCSR0B |= _BV(RXEN0);
 
-#define TIMER_MAX UINT32_MAX
+bool dashButton = 0;
+uint8_t throttle = 0;
+uint8_t brake = 0;
 
-static bool tuned = false;
-static bool locked = false;
-static bool feedback = false;
-
-//uint32_t inputTimer = UINT32_MAX;
-uint32_t buttonTimer = TIMER_MAX;
-uint32_t feedbackTimer = TIMER_MAX;
-
-int buttonCount = 0;
+uint32_t lastButtonPressMillis = 0;
+uint8_t buttonCount = 0;
 uint8_t oldDashButton = 0;
 
 uint16_t calculateChecksum(uint8_t *data)
@@ -63,23 +64,28 @@ uint16_t calculateChecksum(uint8_t *data)
   return sum;
 }
 
-void sendCommand(uint8_t command, uint8_t value, uint8_t address = 0x22, int iterations = 3)
+void sendData(uint8_t address, uint8_t command, uint8_t arg, uint8_t value)
 {
+
+  uint8_t len = 0x04;
+  int iterations = 3;
+
   uint8_t data[] = {
-    0x55, 0xAA, 0x04, address, 0x01, command,
-    0, 0, //value
-    0, 0, //checksum
+      0x55, 0xAA, len, address, command, arg,
+      0, 0, //value
+      0, 0, //checksum
   };
 
-  if (command == COMMAND_SPEED)
-    *(uint16_t *)&data[6] = (value * 252) / 10;
+  if (arg == ARG_MAXSPEED)
+    *(uint16_t *)&data[6] = value * 252 / 10; 
   else
     *(uint16_t *)&data[6] = value;
 
   *(uint16_t *)&data[8] = calculateChecksum(data + 2);
 
   RX_DISABLE;
-  for (int i = 0; i < iterations; i++) {
+  for (int i = 0; i < iterations; i++)
+  {
     Serial.write(data, sizeof(data) / sizeof(uint8_t));
     delay(50);
   }
@@ -88,14 +94,16 @@ void sendCommand(uint8_t command, uint8_t value, uint8_t address = 0x22, int ite
 
 uint8_t readBlocking()
 {
-  while (!Serial.available()) delay(1);
+  while (!Serial.available())
+    delay(1);
   return Serial.read();
 }
 
-uint8_t receivePacket(uint8_t* throttle, uint8_t* brake, uint8_t* dashButton)
+uint8_t receivePacket(uint8_t *throttle, uint8_t *brake, bool *dashButton)
 {
   if (readBlocking() != 0x55)
     return 0;
+
   if (readBlocking() != 0xAA)
     return 0;
 
@@ -115,6 +123,7 @@ uint8_t receivePacket(uint8_t* throttle, uint8_t* brake, uint8_t* dashButton)
 
   uint16_t actualChecksum = (uint16_t)readBlocking() | ((uint16_t)readBlocking() << 8);
   uint16_t expectedChecksum = calculateChecksum(buff);
+
   if (actualChecksum != expectedChecksum)
     return 0;
 
@@ -152,7 +161,7 @@ uint8_t receivePacket(uint8_t* throttle, uint8_t* brake, uint8_t* dashButton)
   break;
   case 0x28:
   {
-    *dashButton = buff[10];
+    *dashButton = (buff[10] == 0x01);
     success = 1;
   }
   break;
@@ -162,12 +171,67 @@ uint8_t receivePacket(uint8_t* throttle, uint8_t* brake, uint8_t* dashButton)
 
 void flashLED(int count, int pause)
 {
-  for (int i = 0; i < count; i++) {
+  for (int i = 0; i < count; i++)
+  {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(pause);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(pause);
+
+    if (i < (count - 1))
+    {
+      delay(pause);
+    }
   }
+}
+
+void blinkEco()
+{
+  sendData(ADDRESS_22, COMMAND_01 ,ARG_ECO, 0x01);
+  delay(250);
+  sendData(ADDRESS_22, COMMAND_01 ,ARG_ECO, 0x00);
+}
+
+void setLock(bool lock)
+{
+  sendData(ADDRESS_22, COMMAND_01 ,ARG_LOCK, lock);
+}
+
+void detune()
+{
+  sendData(ADDRESS_22, COMMAND_01 ,ARG_MAXSPEED, SPEED_LOW);
+  blinkEco();
+}
+
+void setTune()
+{
+  uint8_t tuneLevel;
+
+  if (throttle >= THRESHOLD_HIGH)
+    tuneLevel = SPEED_HIGH;
+  
+  else if (throttle >= THRESHOLD_LOW)
+    tuneLevel = SPEED_MEDIUM;
+  
+  else
+    tuneLevel = SPEED_LOW;
+
+  sendData(ADDRESS_22,COMMAND_01 ,ARG_MAXSPEED, tuneLevel);
+  blinkEco();
+}
+
+void runCommand(uint8_t command)
+{
+  if (command == 3)
+    detune();
+
+  if (command == 4 && brake > THRESHOLD_HIGH)
+    setLock(true);
+
+  if (command == 5)
+    setTune();
+
+  if (command == 6 && brake >= THRESHOLD_LOW && brake <= THRESHOLD_HIGH)
+    setLock(false);
 }
 
 void setup()
@@ -179,147 +243,23 @@ void setup()
 
 void loop()
 {
-  uint8_t throttle = 0;
-  uint8_t brake = 0;
-  uint8_t dashButton = 0;
   uint32_t now = millis();
 
-  if (now - feedbackTimer > 1000 && feedback) {
-    sendCommand(COMMAND_ECO, 0x00);
-    feedbackTimer = TIMER_MAX;
-    feedback = false;
+  receivePacket(&throttle, &brake, &dashButton);
+
+  bool dashButtonHasChanged = dashButton != oldDashButton;
+
+  if (now - lastButtonPressMillis > 1000 && buttonCount > 0)
+  {
+    runCommand(buttonCount);
+    buttonCount = 0;
   }
 
-  if (receivePacket(&throttle, &brake, &dashButton)) {
-    // set speed option by pushing the dashboard button several times
-
-    if (dashButton == 0x01 && oldDashButton == 0x00) { // check oldDashButton to make sure a pressed & hold button is not counted multiple times
-      if (buttonTimer > now) {
-        buttonTimer = now;
-      }
-      oldDashButton = dashButton;
-      buttonCount += 1;
-
-      //taste wird manchmal mehrfach gezählt???
-    }
-
-    if (dashButton == 0x00 && oldDashButton == 0x01) {
-      oldDashButton = dashButton;
-      //flashLED(1, 50);
-    }
-
-    if (now - buttonTimer > 5000) {
-      buttonTimer = TIMER_MAX;
-
-      if (buttonCount == 5) { //speed tuning
-        flashLED(buttonCount, 200);
-
-        if (!tuned) {
-          if (throttle > THRESHOLD_LOW && throttle < THRESHOLD) {
-            sendCommand(COMMAND_SPEED, SPEED_MEDIUM);
-            //flashLED(2, 100);
-          }
-          else if (throttle > THRESHOLD) {
-            sendCommand(COMMAND_SPEED, SPEED_HIGH);
-            //flashLED(5, 100);
-          }
-          tuned = true;
-        }
-        else {
-          sendCommand(COMMAND_SPEED, SPEED_LOW);
-          //flashLED(1, 500);
-          tuned = false;
-        }
-
-        buttonCount = 0;
-        sendCommand(COMMAND_ECO, 0x01);
-        feedbackTimer = now;
-        feedback = true;
-      }
-
-      else if (buttonCount >= 10) {
-        flashLED(buttonCount, 200);
-
-        if (brake > THRESHOLD_LOW && brake < THRESHOLD) {
-          sendCommand(COMMAND_LOCK, 0);
-          locked = 0;
-        }
-        else {
-          sendCommand(COMMAND_LOCK, 1);
-          locked = 1;
-        }
-        buttonCount = 0;
-      }
-
-      flashLED(buttonCount, 200);
-      buttonCount = 0;
-    }
-
-    /*else {
-      if (now - buttonTimer > 3000) {
-        buttonTimer = UINT32_MAX;
-        buttonCount = 0;
-      }
-    }*/
-
-    // Switch locking state:
-    // Press and hold full brake & light throttle for 7 sec to lock or 10 sec to unlock the scooter
-    // Switch tuning state:
-    // fully press and hold both brake and throttle lever for 3 sec to set SPEED_HIGH or 1.5 sec for SPEED_LOW
-
-    /*if (throttle > THRESHOLD_LOW &&
-      throttle < THRESHOLD &&
-      brake > THRESHOLD) {
-      if (inputTimer > now)
-        inputTimer = now;
-
-      if (!locked) {
-        if (now - inputTimer > 7000) {
-          sendCommand(COMMAND_LOCK, 1);
-          flashLED(5, 100);
-          locked = true;
-
-          inputTimer = UINT32_MAX;
-        }
-      }
-      else {
-        if (now - inputTimer > 10000) {
-          sendCommand(COMMAND_LOCK, 0);
-          flashLED(5, 300);
-          locked = false;
-
-          inputTimer = UINT32_MAX;
-        }
-      }
-    }
-
-    else if (throttle > THRESHOLD &&
-      brake > THRESHOLD) {
-      if (inputTimer > now)
-        inputTimer = now;
-
-      if (!tuned) {
-        if (now - inputTimer > 3000) {
-          sendCommand(COMMAND_SPEED, SPEED_HIGH);
-          flashLED(3, 100);
-          tuned = true;
-
-          inputTimer = UINT32_MAX;
-        }
-      }
-      else {
-        if (now - inputTimer > 1500) {
-          sendCommand(COMMAND_SPEED, SPEED_LOW);
-          flashLED(3, 300);
-          tuned = false;
-
-          inputTimer = UINT32_MAX;
-        }
-      }
-    }
-
-    else {
-      inputTimer = UINT32_MAX;
-    }*/
+  if (dashButtonHasChanged && dashButton)
+  {
+    buttonCount += 1;
+    lastButtonPressMillis = now;
   }
+
+  oldDashButton = dashButton;
 }
